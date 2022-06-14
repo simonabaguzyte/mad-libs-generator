e55c8bf2123447128785354aa21e3c04e8ef2df2
Simona <baguzyte98@gmail.com>
2022-06-14 19:34:48 +0300

    an update

--- mad_libs_generator.py
+++ mad_libs_generator.py
@@ -1,7 +1,6 @@
 print("\n****WELCOME TO MAD LIBS GENERATOR!***")
 
-shouldContinue = 1
-while shouldContinue == 1:
+while True:
 
     print("\nPlease, enter a series of inputs below:")
     name1 = input("Name: ")
@@ -12,53 +11,51 @@ while shouldContinue == 1:
     number = input("Number (2-9): ")
     activity = input("Activity: ")
 
+    name1_male = name1.endswith('s')
+    name2_male = name2.endswith('s')
 
-    if name1.endswith('s') and name2.endswith('s'):
-        print("\n\tOne early morning little boy named %s was walkind down the forest to visit \
-his beloved grandfather %s. %s was bringing to his grandfather freshly made %s and %s bottles of %s. \
-The boy had to travel all day because his grandfather %s lived in %s, far far away from him. \
-As soon as %s reached his grandfather's house, he realized that his grandfather %s \
-was not at home - that day he had %s practice. Little boy %s was so tired and upset \
-that he took %s bottles of %s and drunk every last drop of it. After a good nap the boy went back home." \
-% (name1, name2, name1, dish, number, beverage, name2, city, name1, name2, activity, name1, number, beverage))
+    if name1_male and name2_male:
+        print(f"\n\tOne early morning little boy named {name1} was walkind down the forest to visit " +\
+              f"his beloved grandfather {name2}. {name1} was bringing to his grandfather freshly made {dish} and {number} bottles of {beverage}. " +\
+              f"The boy had to travel all day because his grandfather {name2} lived in {city}, far far away from him. " +\
+              f"As soon as {name1} reached his grandfather's house, he realized that his grandfather {name2} " +\
+              f"was not at home - that day he had {activity} practice. Little boy {name1} was so tired and upset " +\
+              f"that he took {number} bottles of {beverage} and drunk every last drop of it. After a good nap the boy went back home.")
 
-    elif name1.endswith('s'):
-        print("\n\tOne early morning little boy named %s was walkind down the forest to visit \
-his beloved grandmother %s. %s was bringing to his grandmother freshly made %s and %s bottles of %s. \
-The boy had to travel all day because his grandmother %s lived in %s, far far away from him. \
-As soon as %s reached his grandmother's house, he realized that his grandmother %s \
-was not at home - that day she had %s practice. Little boy %s was so tired and upset \
-that he took %s bottles of %s and drunk every last drop of it. After a good nap the boy went back home." \
-% (name1, name2, name1, dish, number, beverage, name2, city, name1, name2, activity, name1, number, beverage))
+    elif name1_male:
+        print(f"\n\tOne early morning little boy named {name1} was walkind down the forest to visit " +\
+              f"his beloved grandmother {name2}. {name1} was bringing to his grandmother freshly made {dish} and {number} bottles of {beverage}. " +\
+              f"The boy had to travel all day because his grandmother {name2} lived in {city}, far far away from him. " +\
+              f"As soon as {name1} reached his grandmother's house, he realized that his grandmother {name2} " +\
+              f"was not at home - that day she had {activity} practice. Little boy {name1} was so tired and upset " +\
+              f"that he took {number} bottles of {beverage} and drunk every last drop of it. After a good nap the boy went back home.")
 
-    elif name2.endswith('s'):
-        print("\n\tOne early morning little girl named %s was walkind down the forest to visit \
-her beloved grandfather %s. %s was bringing to her grandfather freshly made %s and %s bottles of %s. \
-The girl had to travel all day because her grandfather %s lived in %s, far far away from her. \
-As soon as %s reached her grandfather's house, she realized that her grandfather %s \
-was not at home - that day he had %s practice. Little girl %s was so tired and upset \
-that she took %s bottles of %s and drunk every last drop of it. After a good nap the girl went back home." \
-% (name1, name2, name1, dish, number, beverage, name2, city, name1, name2, activity, name1, number, beverage))
+    elif name2_male:
+        print(f"\n\tOne early morning little girl named {name1} was walkind down the forest to visit " +\
+              f"her beloved grandfather {name2}. {name1} was bringing to her grandfather freshly made {dish} and {number} bottles of {beverage}. " +\
+              f"The girl had to travel all day because her grandfather {name2} lived in {city}, far far away from her. " +\
+              f"As soon as {name1} reached her grandfather's house, she realized that her grandfather {name2} " +\
+              f"was not at home - that day he had {activity} practice. Little girl {name1} was so tired and upset " +\
+              f"that she took {number} bottles of {beverage} and drunk every last drop of it. After a good nap the girl went back home.")
 
     else:
-        print("\n\tOne early morning little girl named %s was walkind down the forest to visit \
-her beloved grandmother %s. %s was bringing to her grandmother freshly made %s and %s bottles of %s. \
-The girl had to travel all day because her grandmother %s lived in %s, far far away from her. \
-As soon as %s reached her grandmother's house, she realized that her grandmother %s \
-was not at home - that day she had %s practice. Little girl %s was so tired and upset \
-that she took %s bottles of %s and drunk every last drop of it. After a good nap the girl went back home." \
-% (name1, name2, name1, dish, number, beverage, name2, city, name1, name2, activity, name1, number, beverage))
+        print(f"\n\tOne early morning little girl named {name1} was walkind down the forest to visit " +\
+              f"her beloved grandmother {name2}. {name1} was bringing to her grandmother freshly made {dish} and {number} bottles of {beverage}. " +\
+              f"The girl had to travel all day because her grandmother {name2} lived in {city}, far far away from her. " +\
+              f"As soon as {name1} reached her grandmother's house, she realized that her grandmother {name2} " +\
+              f"was not at home - that day she had {activity} practice. Little girl {name1} was so tired and upset " +\
+              f"that she took {number} bottles of {beverage} and drunk every last drop of it. After a good nap the girl went back home.")
 
 
     answer = input("\n\nWould you like to try again (Y/N)? ")
 
-    if answer.startswith('y' or 'Y'):
-        shouldContinue = 1
-    elif answer.startswith('n' or 'N'):
-        shouldContinue = 0
+    if answer.startswith('y') or answer.startswith('Y'):
+        continue
+    elif answer.startswith('n') or answer.startswith('N'):
         print("Thank you for using my program!")
+        break
     else:
         print("Please, check your input and try again")
-        shouldContinue = 2
+        break
         
         
